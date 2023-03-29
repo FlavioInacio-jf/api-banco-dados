01a3a97ed8020534310355c2065d32b7f786bee4
inacio <inacio@pop-os.localdomain>
2023-03-29 08:45:35 -0300

    refactor: Finalizado a API

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
 const express = require("express");
 const { Router } = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const usersDoc = require("./docs/user");
 
@@ -20,17 +21,59 @@ router.get("/", (req, res) => {
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(usersDoc));
 
+let users = [];
 
+const getUserByCPF = (cpf) => {
+  const userExists = users.find((user) => user.cpf === cpf);
+  return userExists;
+}
+
+const addUser = (user) => {
+  const userExists = getUserByCPF(user.cpf);
+
+  if (userExists) {
+    throw new Error("Já existe um usuário cadastrado no sistema com este CPF");
+  }
+  users = [...users, user];
+}
 
 /**
  * Routes
  */
 
-router.get("/users", (req, res) => {
-  res.json({ title: "teste" })
+router.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = getUserByCPF(id)
+    if (!user) {
+      throw new Error("usuário não existe");
+    }
+    return res.json({ result: user })
+  }catch(err){
+    return res.status(404).json({
+      title: "Usuário não encontrado",
+      detail: `${err.message}`
+    })
+  }
 })
 router.post("/users", (req, res) => {
-  res.json({ title: "teste" })
+  const data = req.body;
+
+  try {
+    const user = {
+      _id: uuidv4(),
+      cpf: data.cpf,
+      nome: data.nome,
+      nascimento: data.nascimento,
+    }
+    addUser(user);
+    return res.status(201).json({ title: "usuário criado com sucesso", usuario: user });
+  }catch(err) {
+    return res.status(409).json({
+      title: "Usuário já cadastrado no sistema",
+      detail: `${err.message}`
+    })
+  }
 })
 app.use(router);
 
